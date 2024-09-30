3c6e81f6ddefef95db1560b33ed0bd395a7935fe
duyhiep2519 <duyhiep2519@gmail.com>
2024-09-30 10:35:28 +0700

    set up base

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 // load .env
 dotenv.config();
 
 const app = express();
 
+// use morgan
+app.use(morgan('combined'));
 
 const PORT = process.env.PORT || 3001;
 
