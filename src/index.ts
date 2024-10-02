ef46084f877a6d9df10513cb65e5c063fe0a3a8c
dreamman <huy.nguyen@myg.vn>
2024-10-02 15:22:56 +0700

    save cookie when use app

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser'
 import compression from 'compression';
 import cors from 'cors';
 import { appRouter } from './routes/index';
@@ -17,6 +18,24 @@ app.use(
         limit: '10mb',
     }),
 );
+
+//cookie
+app.use(cookieParser());
+app.use((req, res, next) => {
+    if (!req.cookies.deviceCookie) {
+        // Nếu cookie chưa tồn tại, tạo một cookie mới
+        const deviceId = 'device-' + Math.random().toString(36);
+
+        // Thiết lập cookie với thời hạn 1 năm
+        res.cookie('deviceCookie', deviceId, {
+            maxAge: 365 * 24 * 60 * 60 * 1000, // gia hạn 1 năm cookie
+            httpOnly: true, // Cookie chỉ có thể được truy cập bởi máy chủ
+            sameSite: 'strict' // Cookie chỉ được gửi từ cùng trang web
+        });
+    }
+    next();
+});
+
 
 // use morgan
 app.use(morgan('combined'));
